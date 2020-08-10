ba6932c2e52ce2011daa2fbba63f8a2d95796335
leonorader <dernori@gmail.com>
2020-08-10 20:52:15 +0200

    localtime parse error - heroku fix

--- src/main/java/com/floppylab/salgotarjanschedule/domain/Departure.java
+++ src/main/java/com/floppylab/salgotarjanschedule/domain/Departure.java
@@ -3,6 +3,7 @@ package com.floppylab.salgotarjanschedule.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalTime;
 
@@ -13,6 +14,7 @@ public class Departure {
 
     private long line;
 
+    @DateTimeFormat(pattern = "HH:mm")
     private LocalTime time;
 
     private Day day;
