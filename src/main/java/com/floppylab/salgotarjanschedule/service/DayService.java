7ff5e49461d72c3860290a9eb42a19f998a76c21
leonorader <dernori@gmail.com>
2020-12-14 10:19:42 +0100

    small fix

--- src/main/java/com/floppylab/salgotarjanschedule/service/DayService.java
+++ src/main/java/com/floppylab/salgotarjanschedule/service/DayService.java
@@ -28,8 +28,8 @@ public class DayService {
         if (localDate == null) {
             localDate = LocalDate.now(ZoneId.of("CET"));
         }
-        if (localDate.isBefore(LocalDate.of(2020, 9, 1))
-                || localDate.isAfter(LocalDate.of(2020, 12, 12))) {
+        if (localDate.isBefore(LocalDate.of(2020, 9, 1))) {
+//                || localDate.isAfter(LocalDate.of(2020, 12, 12))) {
             return Collections.emptyList();
         }
         DayInfo info = getDayInfo(localDate);
